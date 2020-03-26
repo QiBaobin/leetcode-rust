e104c703f582fbc31e3f823be0572f47f2d13ecc
Qi, Bob <Baobin.qi@gmail.com>
2020-03-26 19:03:39 +0800

    optimize with single pass linear

--- src/solutions/container_with_most_water.rs
+++ src/solutions/container_with_most_water.rs
@@ -3,53 +3,20 @@ pub struct Solution;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let mut max = 0;
-        let n = height.len();
-        for x in 0..n {
-            let y = height[x] as usize;
-            if y == 0 {
-                continue;
-            }
-            let min_offset = max / y;
-            if x < min_offset && x + min_offset > n - 1 {
-                continue;
-            }
-
-            let (mut left, left_end) = if x >= min_offset {
-                (0, x - min_offset)
+        use std::cmp::{max, min};
+        let mut m = 0;
+        let mut i = 0;
+        let mut j = height.len() - 1;
+        while i < j {
+            m = max(m, (j - i) * (min(height[i], height[j]) as usize));
+            if height[i] < height[j] {
+                i += 1;
             } else {
-                (x, 0)
-            };
-            let mut right = n - 1;
-            let right_end = x + min_offset;
-
-            while left <= left_end || right >= right_end {
-                let offset;
-                if x > left && (right <= x || x - left > right - x) {
-                    if (height[left] as usize) < y {
-                        left += 1;
-                        continue;
-                    }
-                    offset = x - left;
-                } else if right > x {
-                    if (height[right] as usize) < y {
-                        right -= 1;
-                        continue;
-                    }
-                    offset = right - x;
-                } else {
-                    break;
-                }
-
-                let m = offset * y;
-                if m > max {
-                    max = m;
-                }
-                break;
+                j -= 1;
             }
         }
 
-        max as i32
+        m as i32
     }
 }
 
